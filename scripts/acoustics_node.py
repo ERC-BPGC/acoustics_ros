8f2d1c27880aed7061fa2f444f8e15f039c5c129
Ashutosh Gupta <ashutosh.gupta.070801@gmail.com>
2021-11-25 21:58:08 +0530

    Added timer_callback

--- scripts/acoustics_node.py
+++ scripts/acoustics_node.py
@@ -1,29 +1,47 @@
 #! /usr/bin/env python3
 
 import rospy
+from enum import Enum
 from pra_utils import core
 from acoustics_ros.msg import *
 
 # Temporary test parameters NEED TO UPDATE
 path_to_rcf = "../rcf/test.rcf"
+# tf_source_to_mic 
+
+class State(Enum):
+    IDLE = 0
+    RECOMPUTE = 1
+    PUBLISH = 2
 
 class AcousticsNode:
     def __init__(self):
         self.source_pos = Point3D()
         self.mic_pos = Point3D()
-        self.input_signal = Point3DArray()
+        self.input_signal = SignalArray()
+        self.state = State.IDLE
+
+        self.pub = rospy.Publisher('acoustics_node', SignalArray, queue_size=10)
+
+        self.sub_pos = rospy.Subscriber('robot_loc', Point3D, AcousticsNode.position_callback)
+        self.sub_signal = rospy.Subscriber('input_signal', SignalArray, AcousticsNode.signal_callback)
 
     def position_callback(self, msg1):
-        self.source_pos = msg1
-        self.mic_pos = [msg1.x, msg1.y, msg1.z + 0.05]
-
-        self.ComputeWaveform()
+        if msg1 != self.source_pos:
+            # Put transform for source to mic here
+            self.source_pos = msg1
+            self.mic_pos = [msg1.x, msg1.y, msg1.z + 0.05]
+            self.state = State.RECOMPUTE
+        else:
+            self.state = State.PUBLISH
 
     def signal_callback(self, msg1):
-        self.input_signal = msg1
-        
-        self.ComputeWaveform()
-
+        if msg1 != self.input_signal:
+            self.input_signal = msg1
+            self.state = State.RECOMPUTE
+        else:
+            self.state = State.PUBLISH
+    
     def ComputeWaveform(self):
         self.room = core.ComplexRoom.from_rcf(path_to_rcf)
         self.room.add_source(position=self.source_pos, signal=self.input_signal)
@@ -34,16 +52,22 @@ class AcousticsNode:
 
         pub.publish(self.rir)
 
+    def timer_callback(self, event=None):
+        # will check state and either publish or recompute and publish
+        if self.state == State.IDLE:
+            pass
+        elif self.state == State.RECOMPUTE:
+            self.ComputeWaveform()
+        else:
+            self.pub.publish(self.rir)
+
 if __name__ == '__main__':
     try:
         rospy.init_node('acoustics_node', anonymous=True)
-        pub = rospy.Publisher('acoustics_node', Point3DArray, queue_size=10)
-        rate = rospy.Rate(1)  #1hz
+        node = AcousticsNode()
 
         while not rospy.is_shutdown():
             rir = Point3DArray()
-            rospy.Subscriber('robot_loc', Point3D, AcousticsNode.position_callback)
-            rospy.Subscriber('input_signal', Point3DArray, AcousticsNode.signal_callback)
             rate.sleep()
 
     except rospy.ROSInterruptException:
